231b7ff603efe7c241aa6e0f1350f867aebd82e5
Habibur Rahman <hrsshainik@gmail.com>
2023-03-12 21:46:22 +0600

    update login and signup to pt

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,13 +10,13 @@ const Home = () => {
           to="/signup"
           className="mt-6 shadow-sm w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out cursor-pointer"
         >
-          Signup
+          Criar conta
         </Link>
         <Link
           to="/login"
           className="mt-6 shadow-sm w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out cursor-pointer"
         >
-          Login
+          Entrar
         </Link>
       </div>
     </div>
