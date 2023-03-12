4b078d3b71444429803589a9ce7734e8a15c5a53
Habibur Rahman <hrsshainik@gmail.com>
2023-03-12 21:49:46 +0600

    eng to pt

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -14,31 +14,31 @@ const Dashboard = () => {
         {user ? (
           <div>
             <p className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
-              Email: {user.email}
+              E-mail: {user.email}
             </p>
             <button
               onClick={() => signout()}
               className="mt-6 shadow-sm w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out cursor-pointer"
             >
-              Logout
+              Sair
             </button>
           </div>
         ) : (
           <div>
             <p className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
-              User Not Found!
+              Usuário não encontrado!
             </p>
             <Link
               to="/signup"
               className="mt-6 shadow-sm w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out cursor-pointer"
             >
-              Signup
+              Criar conta
             </Link>
             <Link
               to="/login"
               className="mt-6 shadow-sm w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out cursor-pointer"
             >
-              Login
+              Entrar
             </Link>
           </div>
         )}
